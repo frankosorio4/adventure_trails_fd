c51b800213f785198b469378777fc4f74d545735
Frank Osorio <frankosorio4@gmail.com>
2024-04-15 23:22:17 -0300

    adding home page

--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -4,10 +4,18 @@ import "./style.css"
 function CardTrilha({dadosTrilha}){
     return(
         <div className="card_container"> 
-            <img className="card_imagen" src={dadosTrilha.urlImagen} alt="imagen dadosTrilha" />
-            <div className="divDadosTrilha">
-                <h2 className="nomeTrilha">{dadosTrilha.nomeTrilha}</h2>
-                <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+            <img className="card_imagen" src={dadosTrilha.urlImagen} alt="Imagen Trilha" />
+            <div className="div0DadosTrilha">
+                <div className="div1DadosTrilha">
+                    <h2 className="nomeTrilha">{dadosTrilha.nomeTrilha}</h2>
+                    <span className="spanDificuldade">{dadosTrilha.dificuldade}</span>
+                </div>
+                <div className="div2DadosTrilha">
+                    <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+                    <span>Tipo: {dadosTrilha.tipo}</span>
+                    <span>Trajeto: {dadosTrilha.trajeto} Km</span>
+                    <span>Duraçao: {dadosTrilha.duracao} min</span>
+                </div>
             </div>
         </div>
     )
@@ -26,6 +34,5 @@ CardTrilha.propTypes = {
         urlImagen: PropTypes.string
     })
 }
-
 
 export default CardTrilha;
