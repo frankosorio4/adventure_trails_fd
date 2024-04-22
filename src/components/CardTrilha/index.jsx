302286e44b4b36e82a85d4d338d9fabea795b372
Frank Osorio <frankosorio4@gmail.com>
2024-04-21 21:34:54 -0300

    Style changes

--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -5,12 +5,12 @@ function CardTrilha({dadosTrilha}){
     return(
         <div className="card_container"> 
             <img className="card_imagen" src={dadosTrilha.urlImagen} alt="Imagen Trilha" />
-            <div className="div0DadosTrilha">
-                <div className="div1DadosTrilha">
-                    <h2 className="nomeTrilha">{dadosTrilha.nomeTrilha}</h2>
-                    <span className="spanDificuldade">{dadosTrilha.dificuldade}</span>
-                </div>
+            <div className="div1DadosTrilha">
+                <h2 className="nomeTrilha">{dadosTrilha.nomeTrilha}</h2>
                 <div className="div2DadosTrilha">
+                    <div>
+                        <span className="spanDif">{dadosTrilha.dificuldade}</span>
+                    </div>
                     <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
                     <span>Tipo: {dadosTrilha.tipo}</span>
                     <span>Trajeto: {dadosTrilha.trajeto} Km</span>
